2e3edce8b8897836e488879cc9d3000da5dc2ffd
brian24C <brianjosuecastro@hotmail.com>
2023-02-19 16:57:13 -0500

    listo

--- api/index.php
+++ api/index.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <title>Document</title>
 
-    <link rel="stylesheet" type="text/css" href="../static/diseno.css" />
-    <link rel="stylesheet" href="static/diseno.css" />
-
+    <link rel="stylesheet" type="text/css" href="./static/diseno.css" />
 
 
 
